ae9b04f19b0d6dd7bd4122e3c82d771f291a8f21
Michael Thomas (malinka) <malinka@entropy-development.com>
2016-10-06 17:39:26 -0400

    add Logger

--- src/Exception.hh
+++ src/Exception.hh
@@ -6,12 +6,15 @@
 #	define ENTROPY_ASIO_EXCEPTION
 
 #	include <Entropy/Exception.hh>
+#	include <Entropy/Log.hh>
 
 	namespace Entropy
 	{
 		namespace Asio
 		{
 			ENTROPY_EXCEPTION_BASE(Exception, "Base Asio Exception");
+
+			extern Entropy::Log::Logger Log;
 		}
 	}
 
---
+++ src/Logger.cc
@@ -0,0 +1,13 @@
+/*	Copyright 2016 (c) Michael Thomas (malinka) <malinka@entropy-development.com>
+	Distributed under the terms of the GNU Affero General Public License v3
+*/
+
+#include "Exception.hh"
+
+namespace Entropy
+{
+	namespace Asio
+	{
+		Entropy::Log::Logger Log("Asio");
+	}
+}
