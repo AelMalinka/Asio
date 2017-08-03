48024a46d2e241a1a8cf2ed4427f578c605d3b00
Michael Thomas (malinka) <malinka@entropy-development.com>
2017-08-03 13:18:09 -0400

    add Tethys::Timer

---
+++ src/Timer.hh
@@ -0,0 +1,19 @@
+/*	Copyright 2017 (c) Michael Thomas (malinka) <malinka@entropy-development.com>
+	Distributed under the terms of the GNU Affero General Public License v3
+*/
+
+#if !defined ENTROPY_TETHYS_TIMER_INC
+#	define ENTROPY_TETHYS_TIMER_INC
+
+#	include "UV/Timer.hh"
+
+	namespace Entropy
+	{
+		namespace Tethys
+		{
+			// 2017-08-02 AMR TODO: allow build/runtime chosen
+			using UV::Timer;
+		}
+	}
+
+#endif
