a938e7a9a72082f18888b9f6d7b4aa92b80ffb96
Michael Thomas (malinka) <malinka@entropy-development.com>
2018-02-01 02:25:01 -0500

    be explicit on continue condition

--- src/UV/Loop.cc
+++ src/UV/Loop.cc
@@ -135,7 +135,11 @@ void walk(uv_handle_t *handle, void *d)
 	data_t *data = reinterpret_cast<data_t *>(d);
 
 	if(handle->type != UV_SIGNAL) {
-		if(handle->type != UV_TIMER && handle->data != data->timer) {
+		if(handle->type == UV_TIMER) {
+			if(handle->data != data->timer) {
+				data->done = false;
+			}
+		} else {
 			data->done = false;
 		}
 	}
